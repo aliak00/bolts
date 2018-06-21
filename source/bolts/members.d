17840782216595b75431ec5206cf45d2b5156a07
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-06-21 17:07:39 +0200

    add aliases field to members traits

--- source/bolts/members.d
+++ source/bolts/members.d
@@ -5,10 +5,16 @@ module bolts.members;
 
 import bolts.internal;
 
+private template GetMembersAsAliases(T, membersTuple...) {
+    import std.meta: Alias, staticMap, ApplyLeft;
+    alias getMember(T, string name) = Alias!(__traits(getMember, T, name));
+    alias GetMembers(T, names...) = staticMap!(ApplyLeft!(getMember, T), names);
+    alias GetMembersAsAliases = GetMembers!(T, membersTuple);
+}
+
 /// Returns a list of member functions of T
 template memberFunctions(T) {
-    // Get them as an array
-    @property auto array() {
+    auto generateArray() {
         import std.traits: isFunction;
         string[] strings;
         foreach (member; __traits(allMembers, T)) {
@@ -21,10 +27,20 @@ template memberFunctions(T) {
         }
         return strings;
     }
+
+    /// Get as array of immutable strings
+    immutable array = generateArray;
+
+    /// Get as tuple of strings
+    alias tuple = from!"std.meta".aliasSeqOf!(memberFunctions!T.array);
+
+    /// Get as a tuple of aliases
+    alias aliases = GetMembersAsAliases!(T, memberFunctions!T.tuple);
 }
 
 ///
 unittest {
+    import std.meta: AliasSeq;
     struct A {
         void opCall() {}
         void g() {}
@@ -34,28 +50,42 @@ unittest {
         int m;
         A a;
         alias a this;
-        void f() {}
+        void f0() {}
+        void f1() {}
     }
 
-    static assert(is(typeof(memberFunctions!B.array) == string[]));
-    static assert(memberFunctions!B.array == ["f"]);
+    alias array = memberFunctions!B.array;
+    alias tuple = memberFunctions!B.tuple;
+    alias aliases = memberFunctions!B.aliases;
+
+    static assert(array == ["f0", "f1"]);
+    static assert(tuple == AliasSeq!("f0", "f1"));
+
+    static assert(is(typeof(array) == immutable string[]));
+    static assert(is(typeof(tuple) == AliasSeq!(immutable string, immutable string)));
+    static assert(is(typeof(aliases) == AliasSeq!(typeof(B.f0), typeof(B.f1))));
 }
 
 template staticMembers(T) {
     // https://forum.dlang.org/post/duvxnpwnuphuxlrkjplh@forum.dlang.org
-    import std.meta: Filter, staticMap, Alias, AliasSeq, ApplyLeft;
+    import std.meta: Filter, AliasSeq, ApplyLeft;
     import std.traits: hasStaticMember;
-    alias FilterMembers(T, alias Fn) = Filter!(ApplyLeft!(Fn, T), __traits(allMembers, T));
 
-    /// Get as AliasSeq of strings
-    alias tuple = FilterMembers!(T, hasStaticMember);
+    alias FilterMembers(T, alias Fn) = Filter!(ApplyLeft!(Fn, T), __traits(allMembers, T));
 
     /// Get as array of immutable strings
     immutable array = [AliasSeq!(staticMembers!T.tuple)];
+
+    /// Get as tuple of strings
+    alias tuple = FilterMembers!(T, hasStaticMember);
+
+    /// Get as a tuple of aliases
+    alias aliases = GetMembersAsAliases!(T, staticMembers!T.tuple);
 }
 
 unittest {
-    import std.meta: AliasSeq;
+    import std.meta: AliasSeq, Alias;
+    import bolts.meta: TypesOf;
     struct S {
         static void s0() {}
         static int s1 = 3;
@@ -65,12 +95,14 @@ unittest {
         int i = 3;
     }
 
-    alias t = staticMembers!S.tuple;
-    alias a = staticMembers!S.array;
+    alias array = staticMembers!S.array;
+    alias tuple = staticMembers!S.tuple;
+    alias aliases = staticMembers!S.aliases;
 
-    static assert(t == AliasSeq!("s0", "s1", "s2"));
-    static assert(a == ["s0", "s1", "s2"]);
+    static assert(array == ["s0", "s1", "s2"]);
+    static assert(tuple == AliasSeq!("s0", "s1", "s2"));
 
-    static assert(is(typeof(a) == immutable string[]));
-    static assert(is(typeof(t) == AliasSeq!(string, string, string)));
+    static assert(is(typeof(array) == immutable string[]));
+    static assert(is(typeof(tuple) == AliasSeq!(string, string, string)));
+    static assert(is(typeof(aliases) == AliasSeq!(typeof(S.s0), typeof(S.s1), typeof(S.s2))));
 }
